c419ed4a97705fc0b39b6359552d9b5641301457
Chengyu Wu <chengyu-wu1998@outlook.com>
2022-02-09 04:28:18 +0000

    add

--- testflask.py
+++ testflask.py
@@ -1,1 +1,17 @@
+import pytest
+from flask import Flask
 
+app = Flask(__name__)
+
+
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_main(client):
+    rv = client.get("/")
+    assert {"message": "Hello"} == rv.data
